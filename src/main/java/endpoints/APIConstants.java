ccd6fab6ef0dd6514ddc7832bb82b40d22de1213
Harikrishnan-k-k <hkrishnankk@gmail.com>
2024-07-09 22:05:43 +0530

    new

---
+++ src/main/java/endpoints/APIConstants.java
@@ -0,0 +1,10 @@
+package endpoints;
+
+public class APIConstants {
+
+    public static String BaseURL = "https://restful-booker.herokuapp.com";
+    public static String BookingURL = "/booking";
+    public static String AuthgURL = "/auth";
+    public static String PingURL = "/booking";
+
+}
