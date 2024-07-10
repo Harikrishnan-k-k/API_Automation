6d8df2b8b90b5305eb546bfbfa36c1b0ee0282b2
Harikrishnan-k-k <hkrishnankk@gmail.com>
2024-07-10 18:28:27 +0530

    new

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,4 +1,45 @@
 package Base;
 
+import Actions.AssertActions;
+import endpoints.APIConstants;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import modules.payload;
+import org.testng.annotations.BeforeTest;
+
 public class BaseTest {
+    public RequestSpecification requestSpecification;
+    public AssertActions assertActions;
+    public payload payloadManager;
+    public JsonPath jsonPath;
+    public Response response;
+    public ValidatableResponse validatableResponse;
+
+
+    @BeforeTest
+    public void setConfig(){
+        System.out.println("Before test");
+        System.out.println("Before Test");
+        payloadManager = new payload();
+        assertActions = new AssertActions();
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(APIConstants.BaseURL)
+                .addHeader("Content-Type","application/json")
+                .build().log().all();
+
+/*
+        requestSpecification = RestAssured.
+                given()
+                .baseUri(APIConstants.BASE_URL)
+                .contentType(ContentType.JSON)
+                .log().all();
+*/
+    }
+
+    public String getToken(){
+        return null;
+    }
 }
